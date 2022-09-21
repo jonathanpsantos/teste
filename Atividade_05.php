5b86788b4a78b7561c27cae61525ed327a58b8fd
jonathanpsantos <jonathan.santos194@etec.sp.gov.br>
2022-09-21 20:22:13 -0300

    aula_21-09-2022

--- Atividade_05.php
+++ Atividade_05.php
@@ -1,25 +1,37 @@
 <?php 
+	
+	$Idade = 14 ;
+	$sexo = 1 ; 
+	$altura = 1.25 ;
+	$cont ;
+	$maioridade = $Idade + 0;
 
-	$idade ;
-	$sexo;
-	$altura;
+	// Echo "a maioridade é " . $maioridade;
 
 
+	for ($cont=0; $cont <=10 ; $cont++);
 
-do {for ($altura=1.30; $altura <=10 ; $altura++) && ($idade=1.30; $idade <=10 ; $idade++);
+	if ($sexo == 1 ) {
+			
+		$sexo++;
 
+		echo "Você e uma menina ". $cont;
+
+		}
+
+		elseif ($sexo == 2 ) {
+		
+		$sexo++;
+
+		echo "<br/> Matcho alfa ". $cont;
+		}
+		else{
+			echo "Você n tem sexo";
+
+		}
 
 		
-
-	
-
-
-		
-	
-} 
-
-	
-
-
 
  ?>
+
+ 
---
+++ teste.php
@@ -0,0 +1,36 @@
+<?php 
+
+	$idade  ;
+	$idadema ; 
+	$sexof = 1 ;
+	$sexom = 2 ; 
+	$altura  ;
+	$cont ;
+	
+
+	for ($cont=0; $cont <=10 ; $cont++);
+
+	if ($sexof == 1 ) {
+			
+		$sexof++;
+		
+		$idade = 14 ;
+		$idadema = $idade ; 
+
+
+
+		echo $idadema;
+
+		echo "Você e uma menina ". $cont;
+
+
+		}elseif ($sexom == 2 ) {
+		
+		$sexom++;
+
+		echo "<br/> Macho alfa ". $cont;
+		}
+
+
+
+ ?>
